222a01d2ee6d57e6a1541841086edb46247b480b
samyak shrestha <sams.stha@gmail.com>
2019-08-01 16:05:02 +0545

    first try of data fixtures dummy table and dummy data

---
+++ src/UserBundle/DataFixtures/ORM/DataFixtures.php
@@ -0,0 +1,25 @@
+<?php
+namespace UserBundle\DataFixtures\ORM;
+
+use UserBundle\Entity\TestUser;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class DataFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        // create 20 products! Bam!
+        for ($i = 0; $i < 20; $i++) {
+            $product = new TestUser();
+            $product->setName('product '.$i);
+            $product->setAdress('address '.$i);
+            $product->setNumber(mt_rand(10, 100));
+            $manager->persist($product);
+        }
+
+        $manager->flush();
+    }
+}
---
+++ src/UserBundle/Entity/TestUser.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * TestUser
+ *
+ * @ORM\Table(name="test_user")
+ * @ORM\Entity(repositoryClass="UserBundle\Repository\TestUserRepository")
+ */
+class TestUser
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="adress", type="string", length=255)
+     */
+    private $adress;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="number", type="string", length=255)
+     */
+    private $number;
+
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name.
+     *
+     * @param string $name
+     *
+     * @return TestUser
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set adress.
+     *
+     * @param string $adress
+     *
+     * @return TestUser
+     */
+    public function setAdress($adress)
+    {
+        $this->adress = $adress;
+
+        return $this;
+    }
+
+    /**
+     * Get adress.
+     *
+     * @return string
+     */
+    public function getAdress()
+    {
+        return $this->adress;
+    }
+
+    /**
+     * Set number.
+     *
+     * @param string $number
+     *
+     * @return TestUser
+     */
+    public function setNumber($number)
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+
+    /**
+     * Get number.
+     *
+     * @return string
+     */
+    public function getNumber()
+    {
+        return $this->number;
+    }
+}
